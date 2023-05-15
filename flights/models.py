e6d098773aa0fd78d2e3d2e63d5d843bb471ed77
Tanvir <mazumdersaiful1973@gmail.com>
2023-05-15 16:28:51 +0600

    :pray:

---
+++ flights/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(this):
+        return f"{this.city} ({this.code})"
+
+
+
+class Flight(models.Model):
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+
+    def __str__(this):
+        return f"Origin: {this.origin}, Destination: {this.destination}"
+    
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+
+    def __str__(this):
+        return f"{this.first} {this.last}"
