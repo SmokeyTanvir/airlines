7eddae31749938ed84b75ea4da5fb55467f46d10
Tanvir <mazumdersaiful1973@gmail.com>
2023-05-16 16:28:42 +0600

    :pray:

--- airlines2/urls.py
+++ airlines2/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("flights.urls")),
-    path('users/', include("users.urls"))
+    path('', include("flights.urls"))
 ]
